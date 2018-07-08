19a991d6aedaf5e4f7133332805baba576d2f080
Eduardo Rabanal <edurabroj@gmail.com>
2018-07-08 14:16:48 -0500

    feat: Añade LoginActivity como actividad principal

--- app/src/main/java/com/edurabroj/donape/actividades/MainActivity.java
+++ app/src/main/java/com/edurabroj/donape/actividades/MainActivity.java
@@ -1,7 +1,9 @@
-package com.edurabroj.donape;
+package com.edurabroj.donape.actividades;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.edurabroj.donape.R;
 
 public class MainActivity extends AppCompatActivity {
 
