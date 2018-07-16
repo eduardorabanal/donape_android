871ade65096538ddc7455c3589037736798ed4be
Eduardo Rabanal <edurabroj@gmail.com>
2018-07-16 12:23:39 -0500

    fix: Arregla cierre cuando se abre NecesidadDetailsActivity

--- app/src/main/java/com/edurabroj/donape/actividades/NecesidadDetailsActivity.java
+++ app/src/main/java/com/edurabroj/donape/actividades/NecesidadDetailsActivity.java
@@ -2,20 +2,15 @@ package com.edurabroj.donape.actividades;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 
 import com.edurabroj.donape.R;
 
 public class NecesidadDetailsActivity extends AppCompatActivity {
-    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_necesidad_details);
 
-        toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
