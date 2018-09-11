204dda24fa1dbdbb8817479302077ee9d9b5a7cf
Eduardo Rabanal <edurabroj@gmail.com>
2018-09-10 20:36:31 -0500

    fix(localhost server)

--- app/src/main/java/com/edurabroj/donape/shared/data/ApiData.java
+++ app/src/main/java/com/edurabroj/donape/shared/data/ApiData.java
@@ -1,5 +1,5 @@
 package com.edurabroj.donape.shared.data;
 
 public class ApiData {
-    public static final String HOST = "http://192.168.0.15:4000/";
+    public static final String HOST = "http://10.0.2.2:4000/";
 }
